95e1a8e7809fb82fbabcd7d47c64ad13e7d432bb
chaofan-zhai <35815119+chaofan-zhai@users.noreply.github.com>
2020-03-11 13:18:51 -0700

    Add files via upload
    extract facial landmarks in real-time

---
+++ landmark.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 22:13:54 2020
+
+@author: chaofan
+"""
+
+import cv2
+import numpy as np 
+import dlib
+import os 
+#change to your path
+os.chdir(r'C:\Users\chaofan\Desktop\2020 Spring\CS 599\code')
+
+#open your PC's camera and capture its real-time video
+cap = cv2.VideoCapture(0)
+# if you want use a downloaded video 
+#cap = cv2.VideoCapture('./images/Barack Obama.mp4')
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("./predictor/shape_predictor_68_face_landmarks.dat")
+
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        #get landmarks 
+        landmarks = predictor(gray, face)
+
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
+
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
